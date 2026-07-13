1ca747098430ac6cb86b1a96685370877a49b2e5
Vaishnavi Mokle <vaishnavi.mokle@proclink.local>
2026-07-13 15:26:01 +0530

    Use environment PORT for deployment

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.json());
 
 app.use("/transaction", transactionRoutes);
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
